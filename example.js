1fac952a60c94f612905a1580c910409ff56182d
Bastian <bastins321@gmail.com>
2025-04-19 20:39:51 +0800

    menamabhakn readme

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     }
 });
