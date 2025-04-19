338c857de4e0439c32655e1dcd4ba4b8212a2bbd
Bastian <bastins321@gmail.com>
2025-04-19 20:00:55 +0800

    chage auth

--- example.js
+++ example.js
@@ -1,14 +1,8 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const puppeteer = require('puppeteer-core');  // pastikan puppeteer-core digunakan
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: {
-        executablePath: '/usr/bin/chromium-browser', // pastikan path ke chromium sesuai
-        headless: true,  // set headless mode
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
 });
 
 client.on('qr', (qr) => {
